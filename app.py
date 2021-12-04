c50453c3d1c8c94448c0796163ef8610f0cf0c47
Matthew Su <matthew.su.2019@gmail.com>
2021-12-04 02:32:47 -0500

    set up gitignore, index navbar

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+# Template for Flask Heroku app obtained from:
+# https://stackabuse.com/deploying-a-flask-application-to-heroku/
+
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 @app.route('/getmsg/', methods=['GET'])
@@ -44,7 +47,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
