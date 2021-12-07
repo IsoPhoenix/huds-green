5922f422b0a54e5cc25d72ed6eceb5d73f14272a
Jessica Wu <jessicawu@dhcp-10-250-32-244.harvard.edu>
2021-12-07 00:38:18 -0500

    added vgt and vgn fcns

--- app.py
+++ app.py
@@ -60,6 +60,10 @@ def carbon_friendly_menu(date):
 
     menu_df = menu_df.loc[(menu_df['Recipe_Web_Codes'].str.contains("VGT")) & (menu_df["Protein"] >= 5)]
 
+    return menu_df
+
+def grouped_carbon_menu(date)
+    menu_df = carbon_friendly_menu(date)
     grouped = menu_df.groupby("Menu_Category_Name")
     grouped_lists = grouped["Recipe_Print_As_Name"].apply(list).reset_index()
 
@@ -70,6 +74,42 @@ def carbon_friendly_menu(date):
 
     return grouped_lists
 
+def vegetarian():
+    menu_df = carbon_friendly_menu("12/13/2021")
+    menu_df = menu_df.loc[menu_df['Meal_Name'].str.contains("Lunch Entrees")]
+    menu_df = menu_df.sort_values(by='Protein', ascending=False)
+
+    protein = 0
+    length = 0
+    for index, row in menu_df.iterrows():
+        while protein < 50 / 2:
+           protein = protein + pd.to_numeric(row['Protein'])
+           length = length + 1
+
+    vgt_df=menu_df.head(length)
+    vgt_df["Calories"] = pd.to_numeric(vgt_df["Calories"])
+    vgt_df['calorie total'] = vgt_df['Calories'].sum()
+
+    return vgt_df
+
+def vegan():
+    menu_df = carbon_friendly_menu("12/13/2021")
+    menu_df = menu_df.loc[menu_df['Recipe_Web_Codes'].str.contains("VGN")]
+    menu_df = menu_df.loc[menu_df['Meal_Name'].str.contains("Lunch Entrees")]
+    menu_df = menu_df.sort_values(by='Protein', ascending=False)
+
+    protein = 0
+    length = 0
+    for index, row in menu_df.iterrows():
+        while protein < 50 / 2:
+           protein = protein + pd.to_numeric(row['Protein'])
+           length = length + 1
+
+    vgn_df=menu_df.head(length)
+    vgn_df["Calories"] = pd.to_numeric(vgn_df["Calories"])
+    vgn_df['calorie total'] = vgn_df['Calories'].sum()
+
+    return vgn_df
 
 def get_locations(date):
     url = "https://go.apis.huit.harvard.edu/ats/dining/v3/recipes?date={}".format(date)
@@ -110,13 +150,13 @@ def index():
 
     if request.method == "GET":
         # Format menu DF into grouped list by dish category
-        carbon_df = carbon_friendly_menu("12/13/2021")
+        carbon_df = grouped_carbon_menu("12/13/2021")
         lunch_df = menu_grouped("12/13/2021", "Lunch")
         dinner_df = menu_grouped("12/13/2021", "Dinner")
 
         return render_template("index.html", carbon=carbon_df, lunch=lunch_df, dinner=dinner_df, locationList = get_locations("12/13/2021"), currentLocation=get_location_name("05"))
     else:
-        carbon_df = carbon_friendly_menu("12/13/2021")
+        carbon_df = grouped_carbon_menu("12/13/2021")
         lunch_df = menu_grouped("12/13/2021", "Lunch")
         dinner_df = menu_grouped("12/13/2021", "Dinner")
 
