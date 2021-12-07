711ba17a97a0893cc33a2b3200652f4fe8578a75
Matthew Su <matthew.su.2019@gmail.com>
2021-12-06 22:14:49 -0500

    temp

--- app.py
+++ app.py
@@ -41,46 +41,24 @@ def menu_grouped(date, meal):
 
     return grouped_lists
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
+def get_locations(date):
+    url = "https://go.apis.huit.harvard.edu/ats/dining/v3/recipes?date={}".format(date)
 
-    # For debugging
-    print(f"got name {name}")
+    payload={}
+    headers = {
+        'x-api-key': '8yikrfDnvJGbKKlz3pVPvAlANGPkTGza'
+    }
 
-    response = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
 
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+    dataframe = pd.DataFrame.from_dict(response.json())
 
-    # Return the response in json format
-    return jsonify(response)
 
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
+    grouped = dataframe.groupby("Location_Name")
+    grouped_lists = grouped["Location_Number"].apply(list).reset_index()
 
-# A welcome message to test our server
+    return grouped_lists
+
 @app.route('/')
 def index():
 
@@ -88,7 +66,7 @@ def index():
     lunch_df = menu_grouped("12/13/2021", "Lunch")
     dinner_df = menu_grouped("12/13/2021", "Dinner")
 
-    return render_template("index.html", lunch=lunch_df, dinner=dinner_df)
+    return render_template("index.html", lunch=lunch_df, dinner=dinner_df, locationList = get_locations("12/13/2021"))
 
 @app.route("/decarbonize", methods=["GET", "POST"])
 def decarbonize():
@@ -97,21 +75,6 @@ def decarbonize():
     else:
         # Do stuff
         return redirect("/")
-
-# @app.route("/test", methods=["GET"])
-# def test():
-#     url = "https://go.apis.huit.harvard.edu/ats/dining/v3/events?locationId=05&date=12/13/2021"
-
-#     payload={}
-#     headers = {
-#         'x-api-key': '8yikrfDnvJGbKKlz3pVPvAlANGPkTGza'
-#     }
-
-#     response = requests.request("GET", url, headers=headers, data=payload)
-
-#     print(response.json())
-#     return redirect("/")
-
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
